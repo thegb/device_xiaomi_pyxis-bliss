aae3c4ad2501e4af375cfe0d87276c78fd2c63c4
NaassomDourado <naassomdourado@gmail.com>
2020-10-21 03:16:52 +0000

    Revert "pyxis: Selinux Enforcing"
    This reverts commit a9321e223422f177f825faca9eaddff95688d190.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -47,6 +47,7 @@ BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom androidboot.console=ttyMSM0
 BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a600000.dwc3
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += loop.max_part=16
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DTBO := true
